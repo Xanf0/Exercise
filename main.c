db8ff79db7d0478a13ced8e021f55a27317b14df
Xanf0 <145195439+Xanf0@users.noreply.github.com>
2023-12-17 20:26:56 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,39 +1,85 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 4
+#include "functions.h"
+#include "euler.h"
+#define SIZE 2
+int main() {
+ /*
+      {
+        int value = 0;
+        int array[SIZE];
 
-int main() 
-{
-    int RandomNum[SIZE];
-    int guess[SIZE];
-    int bulls, cows;
-    int attempts = 0;
+        printf("value = ");
+        scanf_s("%d", &value);
 
-    GenRandomNum(RandomNum);
+        for (unsigned int i = 0; i < SIZE; ++i)
+            scanf_s("%d", &array[i]);
 
-    do 
-    {
-        printf("\nTry %d\n", attempts + 1);
+        printf("%llu\n", Problem_1(value, array, SIZE));
 
-        for (int i = 0; i < SIZE; i++) 
-        {
-            scanf_s("%1d", &guess[i]);
+        return 0;
+      }*/
+
+      /*
+       int limit;
+        printf("Enter a limit for the Fibonacci sequence value: ");
+        scanf_s("%d", &limit);
+
+        int sum = Problem_2(limit);
+
+        printf("The sum of the even terms of the Fibonacci sequence not exceeding %d is: %d\n", limit, sum);
+
+        return 0;
+    
+      */
+       
+   /*
+    long long number = 600851475143;
+    long long largestPrime = 0;
+
+    for (long long i = 2; i * i <= number; i++) {
+        if (number % i == 0 && Problem_3(i)) {
+            largestPrime = i;
         }
-
-        bulls = Bulls(RandomNum, guess);
-        cows = Cows(RandomNum, guess);
-
-        printf("Bulls: %d, Cows: %d\n", bulls, cows);
-
-        attempts++;
-    } while (bulls != SIZE);
-
-    printf("\nYou won with %d Try.\n", attempts);
-    printf("RandomNum: ");
-    for (int i = 0; i < SIZE; i++) 
-    {
-        printf("%d", RandomNum[i]);
     }
 
+    printf("Most largest del of number its: %lld", largestPrime);
+
     return 0;
+   
+   */
+ 
+   /*
+    int largestPalindrom = 0;
+   
+           for (int i = 100; i < 1000; i++)
+           {
+               for (int j = 100; j < 1000; j++)
+               {
+                   int result = i * j;
+
+                   if (Problem_4(result) && result > largestPalindrom)
+                   {
+                       largestPalindrom = result;
+                   }
+               }
+           }
+
+           printf("answer: %d\n", largestPalindrom);
+    
+   
+   */
+     
+   
+  
+
+    /*
+  int smallestMultiple = Problem_5();
+    printf("The smallest positive number divisible by all numbers from 1 to 20 is: %d", smallestMultiple);
+    return 0;
+    
+    */
+   
+    
 }
+
+
