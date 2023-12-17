ac68500df7547b7c80cb5d171f5667b4bb51e63c
Xanf0 <145195439+Xanf0@users.noreply.github.com>
2023-12-17 20:25:44 +0300

    Add files via upload

--- functions.c
+++ functions.c
@@ -1,6 +1,47 @@
 #include "functions.h"
+#include <time.h>
 
-int Mult2(int value_)
+int GenRandomNum(int RandomNum[SIZE])
 {
-	return value_ * 2;
+    srand(time(NULL));
+
+    RandomNum[0] = rand() % 9 + 1;
+    for (int i = 1; i < SIZE; i++)
+    {
+        RandomNum[i] = rand() % 10;
+    }
 }
+
+int Bulls(int RandomNum[SIZE], int guess[SIZE])
+{
+    int bulls = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (RandomNum[i] == guess[i])
+        {
+            bulls++;
+        }
+    }
+
+    return bulls;
+}
+
+int Cows(int RandomNum[SIZE], int guess[SIZE])
+{
+    int cows = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (RandomNum[i] == guess[j] && i != j)
+            {
+                cows++;
+            }
+        }
+    }
+
+    return cows;
+}
+
--- functions.h
+++ functions.h
@@ -1,1 +1,22 @@
-int Mult2(int value_);
+#pragma once
+#define SIZE 4
+/**
+ * @brief √енерирует случайное число с указанным размером.
+ * @param RandomNum: массив дл€ сохранени€ случайного числа
+ */
+int GenRandomNum(int RandomNum[SIZE]);
+/*
+ * @brief ѕодсчитывает количество совпадающих цифр на правильных позици€х.
+ * @param RandomNum: массив с загаданным числом
+ * @param guess: массив с предполагаемым числом
+ * @return количество быков
+ */
+int Bulls(int RandomNum[SIZE], int guess[SIZE]);
+/*
+ * @brief ѕодсчитывает количество совпадающих цифр, не наход€щихс€ на правильных позици€х.
+ * @param RandomNum: массив с загаданным числом
+ * @param guess: массив с предполагаемым числом
+ * @return количество коров
+ */
+int Cows(int RandomNum[SIZE], int guess[SIZE]);
+
